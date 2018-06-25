68447bcaae121eaad2e07c9df33e017a872912b7
STC214 <stc214@qq.com>
2018-06-25 18:39:39 +0800

    20001

--- result_/list_check3.py
+++ result_/list_check3.py
@@ -1,20 +1,12 @@
-# -*- coding:utf-8 -*-
-
 import os.path
-import sys
+# import sys
 import json
 
-# from ..IOutils import rtfile_input
 
-reload(sys)
-sys.setdefaultencoding('utf-8')
-
-def set_default(obj):
-    if isinstance(obj, set):
-        return list(obj)
-    raise TypeError
-
-# result = json.dumps(yourdata, default=set_default)
+# # from ..IOutils import rtfile_input
+#
+# reload(sys)
+# sys.setdefaultencoding('utf-8')
 
 
 def done_lis():
@@ -25,7 +17,7 @@ def done_lis():
 
     # 获取目录下所有文件
     path = 'F:\\project\\project58\\result_\\'
-    files = os.listdir(path.decode('utf-8'))
+    files = os.listdir(path)
     # 已经有数据的url列表
     done_list = []
     json_list = []
@@ -36,7 +28,7 @@ def done_lis():
             # 准确获取一个txt的位置，利用字符串的拼接
             txt_path = path + file
             # print txt_path
-            text = open(txt_path, 'r')
+            text = open(txt_path, 'r', encoding='utf-8')
             text = text.read()
             dict_s = json.loads(text)
             # print dict_s
@@ -45,14 +37,18 @@ def done_lis():
             # print(type(url))
             done_list.append(url)
             json_list.append(dict_s)
-    # son_list = set(json_list)
 
-    jStr = json.dumps(json_list, ensure_ascii=False, indent=1,default=set_default)
+    son_list = set()
+    print(son_list)
+    json_list = tuple(json_list)
+    son_list.add(json_list)
+    print(son_list)
+    jStr = json.dumps(son_list, ensure_ascii=False, indent=1)
     fd = open('One.json', 'w')
     fd.write(jStr)
     fd.close()
     # print done_list
     return done_list
 
-# 合并json数据到一个文件中
-# done_lis()
+
+done_lis()
